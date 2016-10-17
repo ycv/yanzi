befd4f99f7577835e52a39794bef716a5a3fe4ab
lzb <lzb@lzb-PC>
2016-10-17 09:20:36 +0800

    jquery-paging Signed-off-by:lzb <lzb@lzb-PC>

---
+++ jQuery/jquery-paging/jPaginator/newEmptyPHP.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function getdata($n) {
+    $dataarr = array();
+    for ($i = 0; $i < 5; $i++) {
+        $dataarr[] = $i . "shuju";
+    }
+    return $dataarr;
+}
+
+$d = getdata(1);
+echo "<pre>";
+var_dump($d);
+die;
