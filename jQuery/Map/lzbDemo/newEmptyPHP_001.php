b2339021374aa6b9aced0bcf2f57ea8a97729116
lzb <lzb@lzb-PC>
2016-10-27 17:44:20 +0800

    map edit Signed-off-by:lzb <lzb@lzb-PC>

--- jQuery/Map/lzbDemo/newEmptyPHP_001.php
+++ jQuery/Map/lzbDemo/newEmptyPHP_001.php
@@ -1,34 +1,25 @@
 <?php
 
+/*
+  include_once '../../../PHP/Chinese-characters-to-Pinyin-Class/Pinyin.php';
+  echo Pinyin::getPinyin("腾讯网QQ");
+  echo "<hr>";
+  echo Pinyin::getShortPinyin("腾讯网QQ");
+  echo "<hr>";
+ */
 $setd = array();
 if ($_GET['xllb'] == "2") {
-     
-    //创建对象并打开连接，最后一个参数是选择的数据库名称
-    $mysqli = new mysqli('localhost', 'root', '111111', 'dq');
-    //检查连接是否成功
-    if (mysqli_connect_errno()) {
-        //注意mysqli_connect_error()新特性
-        die('Unable to connect!') . mysqli_connect_error();
+    $sdatas = getdatas();
+    for ($i = 0; $i < count($sdatas); $i++) {
+        $setd[$i]["name"] = $sdatas[$i];
+        $setd[$i]["value"] = ($i + 1) * 97;
+        $setd[$i]["selected"] = false;
     }
-    $sqltxt = "SELECT * FROM `destoon_area_copy` WHERE `parentid` = 0";
-    //执行sql语句，完全面向对象的
-    $result = $mysqli->query($sqltxt);
-    while ($row = $result->fetch_array()) {
-        echo $row['areaname'];
-        die;
-    }
-    /* 关闭连接 */
-    mysqli_close($mysqli);
-
-    for ($i = 0; $i < 10; $i++) {
-        $setd[$i] = $i * 997;
-    }
+    //print_r($setd);die;
 }
 if ($_GET['xllb'] == "1") {
     
 }
-
-//var_dump($setd); die;
 
 $json ['retval'] = true;
 $json ['msg'] = "aaaaaaaaaadads";
@@ -36,3 +27,23 @@ $json ['data'] = $setd;
 
 echo json_encode($json);
 die();
+
+function getdatas() {
+    $d = array();
+    //创建对象并打开连接，最后一个参数是选择的数据库名称
+    $mysqli = new mysqli('localhost', 'root', '111111', 'dq');
+    //检查连接是否成功
+    if (mysqli_connect_errno()) {
+        //注意mysqli_connect_error()新特性
+        die('Unable to connect!') . mysqli_connect_error();
+    }
+    $mysqli->set_charset("utf8");
+    $sqltxt = "SELECT * FROM `destoon_area_copy` WHERE `parentid` = 0 and areaid<50 ";
+    $result = $mysqli->query($sqltxt);
+    while ($row = $result->fetch_array()) {
+        $d[] = $row['areaname'];
+    }
+    /* 关闭连接 */
+    mysqli_close($mysqli);
+    return $d;
+}
