828762595266392f19bccb901cda5a588cadfae8
yan <yan@yan-PC>
2017-06-08 11:43:53 +0800

    vv

--- HTML/Tables/DataTables/lzbdemo/newEmptyPHP.php
+++ HTML/Tables/DataTables/lzbdemo/newEmptyPHP.php
@@ -13,6 +13,7 @@
 
         <script type="text/javascript" language="javascript" class="init">
             
+            
             $(document).ready(function () {
                 $('#example').DataTable({
                     "ajax": "data/newjson.json",
